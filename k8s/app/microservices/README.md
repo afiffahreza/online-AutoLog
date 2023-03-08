c55328e99617eda594ed6ab11bf4dbdf972df19b
afiffahreza <afiffahreza3@gmail.com>
2023-03-08 21:32:42 +0700

    Revamp apps

--- k8s/app/microservices/README.md
+++
@@ -1,3 +0,0 @@
-# Microservice
-
-Microservice used in this project is a simple Microservice Kubernetes Sample found in [GitHub](https://github.com/ewolff/microservice-kubernetes).
--- k8s/local/setup.sh
+++ k8s/local/setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 # Get current directory
 pwd=$(pwd)
@@ -9,12 +10,16 @@ kind create cluster --config $pwd/k8s/local/cluster.yaml
 # Change k8s context to kind
 kubectx kind-autolog
 
-# Apply ingress-nginx controllers & ingress
-kubectl apply -k $pwd/k8s/ingress-nginx/overlays/local
+# Apply loki stack
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo update
+kubectl apply -f $pwd/k8s/loki-stack/namespace.yaml
+helm upgrade --values k8s/loki-stack/values.yaml --install loki -n loki-stack grafana/loki-stack
 
 # Apply apps
-kubectl apply -k $pwd/k8s/app/helloworld/overlays/local
-kubectl apply -k $pwd/k8s/app/echo/overlays/local
+kubectl apply -k $pwd/k8s/app/echo-app/overlays/local
+kubectl apply -k $pwd/k8s/app/error-app/overlays/local
+kubectl apply -k $pwd/k8s/app/list-app/overlays/local
 
-# Apply loki stack
-helm upgrade --values k8s/loki-stack/values.yaml --install loki -n loki-stack grafana/loki-stack
+# Apply ingress-nginx controllers & ingress
+kubectl apply -k $pwd/k8s/ingress-nginx/overlays/local
