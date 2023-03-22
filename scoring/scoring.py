ec19292754468f5978a81eda2b15a7e0f4d6e547
afiffahreza <afiffahreza3@gmail.com>
2023-03-22 11:15:08 +0700

    scoring regex

--- scoring/scoring.py
+++ scoring/scoring.py
@@ -1,34 +1,71 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import explode, split, translate, regexp_replace
-import re
+from pyspark.sql.functions import explode, split, translate, regexp_replace, udf
+from pyspark.sql.types import StringType
 
 spark = SparkSession.builder.appName("AutoLogScoring").getOrCreate()
 
 # Stream from file source on dataset/logs.txt
 lines = spark.read.text("./dataset/Explore-logs-2023-03-15 07_36_39.txt")
 
+
+# ==================== Preprocessing ====================
 # Parse the lines into words, removing the variable tokens of the log lines while preserving the constant parts.
 # We also remove special characters and punctuation, such as #, ?, and %.
 
-# Remove timestamps from the log lines, which are the first 25 characters of each line.
-# example format: 2023-03-15T07:35:42+07:00
-lines = lines.withColumn("value", lines.value.substr(26, 1000))
-
-# Define regular expressions for IP addresses and random log IDs
+# Define regular expressions for variable tokens such as timestamps, IP addresses, random log IDs, and dates
+timestamp_regex = r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\+\d{2}:\d{2})?'
 ip_regex = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(:\d+)?'
 id_regex = r'[0-9a-fA-F]{32}'
 date_regex = r'\d{4}/\d{2}/\d{2}|\d{2}/\w{3}/\d{4}:\d{2}:\d{2}:\d{2} \+\d{4}'
 path_regex = r'/\d{1,4}'
-# Replace IP addresses and random log IDs with fixed tokens
+
+# Replace variables with fixed tokens
+lines = lines.withColumn("value", regexp_replace(lines.value, timestamp_regex, "TIMESTAMP"))
 lines = lines.withColumn("value", regexp_replace(lines.value, ip_regex, "IP"))
 lines = lines.withColumn("value", regexp_replace(lines.value, id_regex, "ID"))
 lines = lines.withColumn("value", regexp_replace(lines.value, date_regex, "DATE"))
-lines = lines.withColumn("value", regexp_replace(lines.value, path_regex, "PATH"))
+lines = lines.withColumn("value", regexp_replace(lines.value, path_regex, "/PATH"))
 
 # Define a string of special characters and punctuation to remove
-special_chars = '!@#$%^&*()_+{}|:"<>?-=[]\;\',./'
+special_chars = '!@#$%^&*()+{}|:"<>?=[]\;\',./'
+
 # Remove special characters and punctuation from the log lines
 lines = lines.withColumn("value", translate(lines.value, special_chars, len(special_chars)*' '))
+
+# Remove double quotes, singular - and _ characters from the log lines
+lines = lines.withColumn("value", regexp_replace(lines.value, r'(\s-\s)|(\s_\s)|"', ' '))
+
+# Replace numbers
+http_status_one_regex = r'\s1[01][0-9]\s'
+http_status_two_regex = r'\s2[02][0-9]\s'
+http_status_three_regex = r'\s30[0-9]\s'
+http_status_four_regex = r'\s4[0-5][0-9]\s'
+http_status_five_regex = r'\s5[01][0-9]\s'
+number_regex = r'\s+\d\s+'
+lines = lines.withColumn("value", regexp_replace(lines.value, http_status_one_regex, " HTTP_STATUS_1xx "))
+lines = lines.withColumn("value", regexp_replace(lines.value, http_status_two_regex, " HTTP_STATUS_2xx "))
+lines = lines.withColumn("value", regexp_replace(lines.value, http_status_three_regex, " HTTP_STATUS_3xx "))
+lines = lines.withColumn("value", regexp_replace(lines.value, http_status_four_regex, " HTTP_STATUS_4xx "))
+lines = lines.withColumn("value", regexp_replace(lines.value, http_status_five_regex, " HTTP_STATUS_5xx "))
+lines = lines.withColumn("value", regexp_replace(lines.value, number_regex, " NUMBER "))
+
+# Remove extra whitespace from the log lines
+lines = lines.withColumn("value", regexp_replace(lines.value, r'\s+', ' '))
+
+# Function to convert all characters to uppercase
+@udf(returnType=StringType()) 
+def upperCase(str):
+    return str.lower()
+
+# Change uppercase characters to lowercase
+lines = lines.withColumn("value", upperCase(lines.value))
+
+
+# ==================== Term weighting ====================
+# Given a chunk after parsing, term weighting is done by:
+# (i) tokening the log lines of the chunk into terms,
+# (ii) counting the occurrences of the terms within the chunk,
+# (iii) computing a numeric score for the chunk based on the occurrences of the terms.
 
 # Split the lines into words
 words = lines.select(
@@ -37,21 +74,17 @@ words = lines.select(
     ).alias("word")
 )
 
-# Term weighting
-# Given a chunk after parsing, term weighting is done by:
-# (i) tokening the log lines of the chunk into terms,
-# (ii) counting the occurrences of the terms within the chunk,
-# (iii) computing a numeric score for the chunk based on the occurrences of the terms.
-# Tokenization is applied to all the lines, including those pertaining to negations in the log files.
-
 # Count the occurrences of the terms within the chunk
 wordCounts = words.groupBy("word").count()
 
 # Output the results
 wordCounts.show()
 
-# Save the result to a file, overwriting the existing file
+# Save the result to a csv file, overwriting the existing file
 wordCounts.coalesce(1).write.mode("overwrite").format("com.databricks.spark.csv").option("header", "true").save("output")
 
-# Stop the Spark session when done
+# Output the result to a json file, overwriting the existing file
+# wordCounts.coalesce(1).write.mode("overwrite").json("json_output")
+
+# Stop the Spark session
 spark.stop()
