9de212c07ddb2819552f588e19bfe11fbf57f582
afiffahreza <afiffahreza3@gmail.com>
2023-03-31 11:27:43 +0700

    init db scoring local deployment

--- scripts/setup-local.sh
+++ scripts/setup-local.sh
@@ -19,5 +19,13 @@ kubectl apply -k k8s/microservices/overlays/local
 # Apply ingress-nginx controllers & ingresses
 kubectl apply -k k8s/ingress-nginx/overlays/local
 
+# Apply couchdb for autolog
+helm repo add couchdb https://apache.github.io/couchdb-helm
+helm repo update
+helm upgrade --values k8s/online-autolog/couchdb/values.yaml --install couchdb --version=3.3.1 -n online-autolog couchdb/couchdb
+
 # Print grafana password
 echo "Grafana password: $(kubectl get secret loki-grafana -n loki-stack -o jsonpath="{.data.admin-password}" | base64 --decode ; echo)"
+
+# Print couchdb password
+echo "Couchdb password: $(kubectl get secret couchdb -n online-autolog -o jsonpath="{.data.adminPassword}" | base64 --decode ; echo)"
