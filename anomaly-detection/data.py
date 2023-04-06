0bd6f98c441caed962c2ac3495a0ccb0dcd7d1f2
afiffahreza <afiffahreza3@gmail.com>
2023-04-07 02:45:06 +0700

    test data for anomaly detection

--- anomaly-detection/data.py
+++ anomaly-detection/data.py
@@ -13,13 +13,66 @@ def retrieve_data(db, applications):
         db_name = "train-" + app
         current_data = db.get_last(db_name)
         data[app] = current_data['data']['score']
+    max_len = max([len(v) for v in data.values()])
+    data['label'] = [0 for i in range(max_len)]
     return pd.DataFrame.from_dict(data)
 
+def retrieve_data_scoring(db, applications):
+    data = {}
+    for app in applications:
+        db_name = "score-" + app
+        docs = []
+        num = int(db.get_docs_count(db_name))  
+        for i in range(1, num+1):
+            doc = db.get(db_name, str(i))
+            docs.append(doc['data']['score'])
+        data[app] = docs
+    data['label'] = [0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    return pd.DataFrame.from_dict(data)
 
 if __name__ == '__main__':
     db = CouchDB('http://localhost:5984', 'admin', 'password')
     applications = ['catalog', 'customer', 'order', 'ingress-nginx']
-    print(retrieve_data(db, applications))
+    df_train = retrieve_data(db, applications)
+    df_test = retrieve_data_scoring(db, applications)
+    
+    print(df_train)
+    print(df_test)
+
+    x_train = df_train.drop('label', axis=1)
+    y_train = df_train['label']
+    x_test = df_test.drop('label', axis=1)
+    y_test = df_test['label']
+
+    scaler = MinMaxScaler()
+    x_train = scaler.fit_transform(x_train)
+    x_test = scaler.transform(x_test)
+
+    print(x_train)
+    print(y_train)
+    print(x_test)
+    print(y_test)
+
+    from sklearn.svm import OneClassSVM
+
+    clf = OneClassSVM(nu=0.1, kernel="rbf", gamma=0.1)
+
+    clf.fit(x_train)
+
+    y_pred_train = clf.predict(x_train)
+    y_pred_train[y_pred_train == 1] = 0
+    y_pred_train[y_pred_train == -1] = 1
+
+    print(y_train.values)
+    print(y_pred_train)
+
+    y_pred_test = clf.predict(x_test)
+    y_pred_test[y_pred_test == 1] = 0
+    y_pred_test[y_pred_test == -1] = 1
+
+    print(y_test.values)
+    print(y_pred_test)
+
 
 #BG/L
 names = ['ID','NULL','R00','R01','R02','R03','R04','R05','R06','R07','R10','R11','R12','R13','R14','R15','R16','R17','R20',
