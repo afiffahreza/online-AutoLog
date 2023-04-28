94e211f2f3b5d9fac15114fa6ec1924b329dea3d
afiffahreza <afiffahreza3@gmail.com>
2023-04-28 13:00:23 +0700

    test log parsing drain3

--- scoring/app/parsing.py
+++ scoring/app/parsing.py
@@ -2,10 +2,11 @@
 
 import iplom as IPLoM
 import Spell
+import drain as Drain
 
 input_dir    = './dataset/'  # The input directory of log file
 output_dir   = './dataset/result/'  # The output directory of parsing results
-log_file     = 'logs_test.txt'  # The input log file name
+log_file     = 'order-log.txt'  # The input log file name
 log_format   = '<Date> <Content>'  # BGL log format
 # maxEventLen  = 200  # The maximal token number of log messages (default: 200)
 # step2Support = 0  # The minimal support for creating a new partition (default: 0)
@@ -17,12 +18,15 @@ log_format   = '<Date> <Content>'  # BGL log format
 # parser = IPLoM.LogParser(log_format=log_format, indir=input_dir, outdir=output_dir,
 #                          maxEventLen=maxEventLen, step2Support=step2Support, CT=CT, 
 #                          lowerBound=lowerBound, upperBound=upperBound, rex=regex)
-tau        = 0.5  # Message type threshold (default: 0.5)
+# tau        = 0.5  # Message type threshold (default: 0.5)
 ip_regex = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(:\d+)?'
 id_regex = r'[0-9a-fA-F]{32}'
 hexa_regex = r'0x[0-9a-fA-F]+'
 regex      = [ip_regex, id_regex, hexa_regex]  # Regular expression list for optional preprocessing (default: [])
 
-parser = Spell.LogParser(indir=input_dir, outdir=output_dir, log_format=log_format, tau=tau, rex=regex)
+# parser = Spell.LogParser(indir=input_dir, outdir=output_dir, log_format=log_format, tau=tau, rex=regex)
+st         = 0.5  # Similarity threshold
+depth      = 4  # Depth of all leaf nodes
 
+parser = Drain.LogParser(log_format, indir=input_dir, outdir=output_dir,  depth=depth, st=st, rex=regex)
 parser.parse(log_file)
