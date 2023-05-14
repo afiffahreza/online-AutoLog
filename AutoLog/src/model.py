23cb9f9604c8618c64ab2a8c62fa3d9e9b4134b2
afiffahreza <afiffahreza3@gmail.com>
2023-05-14 15:35:52 +0700

    finalize training validation

--- AutoLog/src/model.py
+++ AutoLog/src/model.py
@@ -3,15 +3,12 @@ import pandas as pd
 import matplotlib as mpl
 import tensorflow as tf
 import matplotlib.pyplot as plt
-import seaborn as sns
 from keras.models import Model
-from keras.utils import plot_model
 from keras.layers import Input, Dense, Dropout
 from keras import regularizers, initializers
-from sklearn.metrics import confusion_matrix, roc_curve
-from pathlib import Path
-from numpy import sqrt, argmax
+from sklearn.metrics import confusion_matrix
 from time import time
+import pickle
 
 mpl.use('TkAgg')
 np.random.seed(4999)
@@ -26,56 +23,36 @@ class MultilayerAutoEncoder():
         layer = Dense(128, activation='relu',   #128
                         activity_regularizer=regularizers.l1(10e-5),
                         kernel_initializer=initializers.RandomNormal())(input_layer)
-
-        #v2
+        
         layer = Dropout(rate=0.6)(layer);
-        #layer = Dropout(rate=0.5)(layer);
-
-        #layer = Dropout(rate=0.5)(input_layer);
-
 
         layer = Dense(64, activation='tanh',    #64
                       activity_regularizer=regularizers.l1(10e-5),
-                      kernel_initializer=initializers.RandomNormal())(layer)# RandomNormal())(layer)
-
-        #v2
+                      kernel_initializer=initializers.RandomNormal())(layer)
+        
         layer = Dropout(rate=0.6)(layer);
-        #layer = Dropout(rate=0.5)(layer);
 
         layer = Dense(128, activation='relu',   #128
                       activity_regularizer=regularizers.l1(10e-5),
                       kernel_initializer=initializers.RandomNormal())(layer)
-
+        
         output_layer = Dense(input_dim, activation='relu',
                       activity_regularizer=regularizers.l1(10e-5),
                       kernel_initializer=initializers.RandomNormal())(layer)
 
-
-
         self.autoencoder = Model(inputs=input_layer, outputs=output_layer)
-
-        # plot_model(self.autoencoder, to_file='model_plot.png', show_shapes=True,
-        #            show_layer_names=True)  # , rankdir='LR')
 
     def summary(self, ):
         self.autoencoder.summary()
 
     def train(self, x, y):
 
-
-        #epochs = 50
-        #v2
         epochs = 50
         batch_size = 2048
-        #v1
-        #epochs = 100
-        #batch_size = 4
         validation_split = 0.1
 
         print('Start training.')
 
-        #lr = 0.0002
-        #print('lr:', lr)  # Adagrad    #sgd    #adadelta  #rmsprop
         self.autoencoder.compile(optimizer='rmsprop',
                                  loss='mean_squared_error')
         start = time()
@@ -86,102 +63,43 @@ class MultilayerAutoEncoder():
                                        validation_split=validation_split,
                                        verbose=2)
         print(time() - start)
-        plt.plot(history.history['loss'])
-        plt.plot(history.history['val_loss'])
-        plt.title('model loss')
-        plt.ylabel('loss')
-        plt.xlabel('epoch')
-        plt.legend(['train', 'test'], loc='upper left')
-        plt.show()
 
-
-        # --- determine treshold ---
         x_val = x[x.shape[0]-(int)(x.shape[0]*validation_split):x.shape[0]-1, :]
 
         print(' + validation_size    : ',  x_val.shape)
         print(x_val)
-        # print (x_val[1])
-        # print(x_val[2])
 
         val_predictions = self.autoencoder.predict(x_val)
         val_mse = np.mean(np.power(x_val - val_predictions, 2), axis=1)
 
-        #threshold = np.median ( val_mse )   #np.mean ( val_mse ) + 3*np.std ( val_mse )
-        threshold = np.percentile(val_mse , 90) #np.mean ( val_mse ) + np.std ( val_mse )
+        threshold = np.percentile(val_mse , 90)
         print('Current threshold: ')
         print(threshold)
-        # ---
 
         plt.plot(val_mse)
         plt.show()
 
         df_history = pd.DataFrame(history.history)
         return df_history, threshold
+    
+    def predict(self, x, threshold):
+        predictions = self.autoencoder.predict(x)
+        mse = np.mean(np.power(x - predictions, 2), axis=1)
+        y_pred = [1 if e > threshold else 0 for e in mse]
+        return y_pred
 
     def evaluate(self, x_test, y_test, threshold):
         predictions = self.autoencoder.predict(x_test)
 
         mse=np.mean (np.power(x_test - predictions, 2), axis=1)
-
-        #mse = np.sum(np.power(x_test - predictions, 2), axis = 1)#
-        #mse = abs (  np.min(x_test - predictions, axis=1) )
         y_test = y_test.reset_index(drop=True)
         df_error = pd.DataFrame({'reconstruction_error' : mse, 'true_class' : y_test})
         print(mse)
-        #y_test = y_test.reset_index(drop=True)
         print('y_test***: ')
-        #pd.option_context("display.max_rows", None, "display.max_columns", None)
         print(df_error.to_string())
         print(df_error.describe(include='all'))
 
-        # plot_reconstruction_error(df_error, threshold)
         compute(df_error, threshold)
-
-        # plot_thresold(df_error.true_class, df_error.reconstruction_error)
-
-
-
-
-from pathlib import Path
-base_dir = str(Path().resolve().parent)
-def plot_reconstruction_error(errors, threshold):
-    groups = errors.groupby('true_class')
-    a_list = list(range(1, 373))
-    fig, ax = plt.subplots(figsize=(5, 7))
-    right = 0
-    a_list = list(range(1, 141))
-    for name, group in groups:
-        #print('Index: ')
-        #print(group.index)
-        if max(group.index) > right: right = max(group.index)
-
-        ax.plot(group.index, group.reconstruction_error,
-                ms = 5, linestyle = '', markeredgecolor = 'black', markersize=5,
-                label = 'Normal' if int(name) == 0 else 'Anomaly', marker ='o' if int(name) == 0 else 'v', color = 'lightgreen' if int(name) == 0 else 'red')
-        a_list = list(range(1, 233))
-    ax.hlines(threshold, ax.get_xlim()[0], ax.get_xlim()[1], colors = 'red', zorder = 100, label = 'Threshold',linewidth=2,linestyles='dashed')
-    ax.semilogy()
-    ax.legend()
-    plt.minorticks_off()
-    #ax.set_xticks([754])
-    #ax.set_xticklabels(["754"])
-    ax.set_xticklabels([])
-    BIGGER_SIZE = 22
-    plt.rc('axes', labelsize=BIGGER_SIZE)
-    plt.rc('ytick', labelsize=BIGGER_SIZE)
-    plt.xlim(left = 0, right = right)
-    #plt.ylim(bottom= 0.00003, top=1000)
-    plt.yticks(fontsize=22)
-    ax.legend(prop={'size': 15}, loc='lower right' )
-    #plt.legend(bbox_to_anchor=(1, 1), loc='upper right')
-    plt.title('Reconstruction error for different classes')
-    #plt.grid(True)
-    plt.ylabel('Reconstruction error')
-    plt.xlabel('Data point index')
-    plt.savefig(base_dir + '/reconstruction_error.png', bbox_inches = 'tight', dpi = 500)
-    #plt.figure(1)
-    plt.show()
-
 
 def compute(df_error, threshold):
     y_pred = [1 if e > threshold else 0 for e in df_error.reconstruction_error.values]
@@ -199,36 +117,11 @@ def compute(df_error, threshold):
     print('F1 = ', f1);
     print('false alarm = ', false_alarm);
 
-    sns.heatmap(conf_matrix, xticklabels=['Normal', 'Attack'], yticklabels=['Normal', 'Attack'], annot=True,fmt='d');
-    plt.title('Confusion matrix')
-    plt.ylabel('True class')
-    plt.xlabel('Predicted class')
-    # plt.savefig(base_dir + '/confusion_matrix.png', bbox_inches='tight', dpi=500)
-    #plt.figure(2)
-    plt.show()
-    #fig2.show()
+def save_model(model, path):
+    with open(path, 'wb') as f:
+        pickle.dump(model, f)
 
-
-
-def plot_thresold(true_class, re):
-
-    # new Threshold
-    # calculate roc curve
-    fpr, tpr, thresholds = roc_curve(true_class, re)
-    # calculate the g-mean for each threshold
-    gmeans = sqrt(tpr * (1 - fpr))
-    # locate the index of the largest g-mean
-    ix = argmax(gmeans)
-    print('Best Threshold=%f, G-Mean=%.3f' % (thresholds[ix], gmeans[ix]))
-    # plot the roc curve for the model
-    plt.plot([0, 1], [0, 1], linestyle='--', label='No Skill')
-    plt.plot(fpr, tpr, marker='.', label='AutoLog')
-    plt.scatter(fpr[ix], tpr[ix], marker='o', color='black', label='Best')
-    # axis labels
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.legend(prop={'size': 17})
-    # show the plot
-    plt.show()
-    # show the plot
-    plt.show()
+def load_model(path):
+    with open(path, 'rb') as f:
+        model = pickle.load(f)
+    return model
---
+++ AutoLog/validate_training.py
@@ -0,0 +1,245 @@
+import pandas as pd
+from datetime import datetime, timedelta
+from src.logger import preprocess
+from src.scoring import Scoring
+from os.path import dirname
+from src.model import MultilayerAutoEncoder, save_model
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
+from drain3 import TemplateMiner
+from drain3.template_miner_config import TemplateMinerConfig
+from drain3.file_persistence import FilePersistence
+
+if __name__ == "__main__":
+
+    print("Starting training...")
+    print("Time: " + str(datetime.now()))
+
+    applications = ['R00','R01','R02','R03','R04','R05','R06','R07','R10','R11','R12','R13','R14','R15','R16','R17','R20',
+        'R21','R22','R23','R24','R25','R26','R27','R30','R31','R32','R33','R34','R35','R36','R37','R40','R41','R42','R43',
+        'R44','R45','R46','R47','R50','R51','R52','R53','R54','R55','R56','R57','R60','R61','R62','R63','R64','R65',
+        'R66','R67','R70','R71','R72','R73','R74','R75','R76','R77','UNKNOWNLOCATION','UNSPECIFIED']
+    log_period = 300
+
+    # Read log file
+    log_file = open("dataset/BGL.log", "r")
+    
+    # Log Structure
+    # <log type> <id> <date> <logging entity> <datetime> <logging entity> <log>
+    # date format: YYYY.MM.DD
+    # logging entity format: RXX-{unimportant string} or UNKNOWN_LOCATION for 'UNKNOWNLOCATION' or NULL for 'UNSPECIFIED'
+    # datetime format: YYYY-MM-DD-HH.MM.SS.mmmmmm
+
+    # Split log file into lines
+    log_lines = log_file.read().splitlines()
+
+    # Close log file
+    log_file.close()
+
+    # Group log lines by logging entity and datetime using P=300s
+    # Log time full 2005-06-03-15.42.50.363779 until 2006-01-04-08.00.05.233639
+    # Time iteration: 2005-06-03-15.40 - 2005-06-03-15.45, ..., 2006-01-04-08.00 - 2006-01-04-08.05
+    # Total time iterations: ~ 288 * 180 = 51840
+    time_iteration_dict = {}
+    start_time = datetime.strptime("2005-06-03-15.40.00", "%Y-%m-%d-%H.%M.%S")
+    end_time = datetime.strptime("2006-01-04-08.05.00", "%Y-%m-%d-%H.%M.%S")
+    time_iteration = 0
+    while start_time < end_time:
+        # print("Time iteration: " + str(time_iteration) + " - " + str(start_time) + " - " + str(start_time + timedelta(seconds=log_period)))
+        time_iteration_dict[time_iteration] = (start_time, start_time + timedelta(seconds=log_period))
+        start_time += timedelta(seconds=log_period)
+        time_iteration += 1
+
+    # print count time iteration
+    print("Time iteration count: " + str(time_iteration))
+    log_per_time_per_entity = {}
+    # log_group_datetime = None
+    time_iteration = 0
+
+    # print("Creating default scoring dict...")
+    # print("Time: " + str(datetime.now()))
+    for time_iteration in time_iteration_dict:
+        log_per_time_per_entity[time_iteration] = {}
+        for application in applications:
+            log_per_time_per_entity[time_iteration][application] = []
+        log_per_time_per_entity[time_iteration]['label'] = 'OK'
+    # print("Done creating default scoring dict...")
+    # print("Time: " + str(datetime.now()))
+
+
+    persistence = FilePersistence("drain3_state.bin")
+
+    config = TemplateMinerConfig()
+    config.load(dirname(__file__) + "/drain3.ini")
+    config.profiling_enabled = False
+
+    template_miner = TemplateMiner(persistence, config)
+
+    time_iteration = 0
+    label = 'OK'
+    # error_lines = {}
+    for log_line in log_lines:
+        log = log_line.split(" ")
+        log_datetime = datetime.strptime(log[4], "%Y-%m-%d-%H.%M.%S.%f")
+        current_entity = log[3][0:3]
+        if label == 'OK' and log[0] == '-':
+            label = 'OK'
+        else:
+            label = 'ALERT'
+        if current_entity == "UNK":
+            current_entity = "UNKNOWNLOCATION"
+        elif current_entity == "NUL":
+            current_entity = "UNSPECIFIED"
+        log_line_without_first_six = " ".join(log[6:])
+        template_miner.add_log_message(log_line_without_first_six)
+        # if log_group_datetime is None:
+        #     log_group_datetime = log_datetime
+        while log_datetime > time_iteration_dict[time_iteration][1]:
+            if log[0] == '-':
+                label = 'OK'
+            time_iteration += 1
+        # if log_datetime - log_group_datetime > timedelta(seconds=log_period):
+        #     if log[0] == '-':
+        #         label = 'OK'
+        #     log_group_datetime = log_datetime
+        #     time_iteration += 1
+        if time_iteration not in log_per_time_per_entity:
+            log_per_time_per_entity[time_iteration] = {}
+        if current_entity not in log_per_time_per_entity[time_iteration]:
+            log_per_time_per_entity[time_iteration][current_entity] = []
+        log_per_time_per_entity[time_iteration][current_entity].append(log_line_without_first_six)
+        log_per_time_per_entity[time_iteration]['label'] = label
+        # if label == 'ALERT':
+        #     if time_iteration not in error_lines:
+        #         error_lines[time_iteration] = []
+        #     error_lines[time_iteration].append(log_line)
+
+    # Preprocess log lines
+    preprocessed_log_per_time_per_entity = {}
+    for time_iteration in log_per_time_per_entity:
+        for entity in log_per_time_per_entity[time_iteration]:
+            if entity == 'label':
+                continue
+            if time_iteration not in preprocessed_log_per_time_per_entity:
+                preprocessed_log_per_time_per_entity[time_iteration] = {}
+            templates = []
+            for template in log_per_time_per_entity[time_iteration][entity]:
+                # print(template)
+                result = template_miner.match(template)
+                if result is None:
+                    # print("none")
+                    templates.append('')
+                else:
+                    # print("hit")
+                    templates.append(result.get_template())
+            # print(templates)
+            preprocessed_log_per_time_per_entity[time_iteration][entity] = preprocess(templates)
+            # print(preprocessed_log_per_time_per_entity[time_iteration][entity])
+            # if time_iteration in error_lines:
+            #     print("Time iteration: " + str(time_iteration))
+            #     print("Entity: " + entity)
+            #     print("Log lines: " + str(preprocessed_log_per_time_per_entity[time_iteration][entity]))
+
+    # Save template miner
+    template_miner.save_state("Saving template miner...")
+
+    # Print preprocessed log lines
+    # for time_iteration in preprocessed_log_per_time_per_entity:
+    #     for entity in preprocessed_log_per_time_per_entity[time_iteration]:
+    #         print("Time iteration: " + str(time_iteration))
+    #         print("Entity: " + entity)
+    #         print("Log lines: " + str(preprocessed_log_per_time_per_entity[time_iteration][entity]))
+    #         print("")
+    
+    # Score log lines per entity
+    scores = {}
+    for entity in applications:
+        scoring = Scoring()
+        for time_iteration in preprocessed_log_per_time_per_entity:
+            # print(entity)
+            # print(preprocessed_log_per_time_per_entity[time_iteration])
+            if entity in preprocessed_log_per_time_per_entity[time_iteration]:
+                scoring.add_lines(lines=preprocessed_log_per_time_per_entity[time_iteration][entity])
+            else:
+                scoring.add_lines([])
+        scores[entity] = scoring.calculate_baseline_score()
+        scoring.save("./output/test230514/scoring_" + entity + ".pkl")
+    
+    # Add id and label to scores
+    scores['Label'] = []
+    scores['Id'] = []
+    for time_iteration in log_per_time_per_entity:
+        scores['Label'].append(log_per_time_per_entity[time_iteration]['label'])
+        scores['Id'].append(time_iteration)
+        
+    # Print scores
+    # for entity in applications:
+    #     print("Entity: " + entity)
+    #     print("Scores: " + str(scores[entity]))
+    #     print("")
+
+    # Create pandas dataframe
+    df = pd.DataFrame(scores)
+    # add dummy column
+    df['dummy'] = 0
+    df.head()
+
+    # order last 2 column to first 2
+    cols = df.columns.tolist()
+    cols = cols[-2:] + cols[:-2]
+    df = df[cols]
+    df.head()
+
+    # save to csv
+    df.to_csv("./output/test230514/scoring.csv", index=False)
+
+    # Create error log
+    # error_log = open("dataset/error_log.log", "w")
+    # for time_iteration in error_lines:
+    #     error_log.write("Time iteration: " + str(time_iteration))
+    #     error_log.write("\n")
+    #     for line in error_lines[time_iteration]:
+    #         error_log.write(line)
+    #         error_log.write("\n")
+    #     error_log.write("\n")
+
+    df['Label'] = np.where(df['Label'] == 'OK', 0, 1)
+
+    # Split anomalous and normal data
+    good = df[df['Label'] == 0]
+    bad = df[df['Label'] == 1]
+
+    # Split good data into train and test
+    x_train, x_test = train_test_split(good, test_size=0.016, random_state=345)
+
+    # Concat bad data to test
+    x_test = pd.concat([x_test, bad])
+
+    # Split data into features and labels
+    y_train = x_train['Label']
+    y_test = x_test['Label']
+    x_train = x_train.drop(['Label', 'dummy'], axis=1)
+    x_test = x_test.drop(['Label', 'dummy'], axis=1)
+
+    # Convert to numpy array
+    x_train = x_train.values
+    x_test = x_test.values
+
+    # Scale data
+    scaler = MinMaxScaler()
+    x_train_scaled = scaler.fit_transform(x_train)
+    x_test_scaled = scaler.transform(x_test)
+
+    # Train model
+    input_dim = x_train.shape[1]
+    autoencoder = MultilayerAutoEncoder(input_dim = input_dim)
+    autoencoder.summary()
+    history, threshold = autoencoder.train(x_train_scaled, x_train_scaled)
+
+    # save model
+    save_model(autoencoder, "./output/test230514/model.pkl")
+
+    print("Finished training.")
+    print("Time: " + str(datetime.now()))
